831e3b77b134e30af1ea7c1c426c7086c5fe29f4
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-01-03 22:19:57 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,33 @@
+<?php
+echo $red."[!] warning !!!".$t;
+echo $kuning."[-] ini adalah program ilegal".$t;
+echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+$mumet = $putih.'[*] '.$ijo.'mumet bos '.$putih.'[-] code: '.$ijo.'T2WJQH';
+$ads= $putih."[÷]    official channel    [÷]".$t.$ijo."[-] efte chanel ".$putih."[-]";
+$msgtoto = "[!] Ngopi dulu bos".$t.$red."[°] demi keamanan hentikan aktifitas bot sekarang !!!!".$t.$kuning."[•] ctrl + c";
+$msgyd = "[!] ganguan pada server jangan panik ini tidak lama";
+$yd_stat = true;
+$vip1= false;
+$weblock =[""];
+$ydblock = [""];
+$we_stat = true;
+$msgwe = "[!] enjoy your life";
+$msgveeu = "[!] enjoy your life";
+
+$data= [
+'irina toderr',
+ 
+//vipm
+'testing',
+ 
+ //mpiv
+'testing',
+
+/*
+ 'testing',
+ */
+ 'testing',
+];
+
+
+$ydnews = ['tes','eftechanel',];
