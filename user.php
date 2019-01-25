7e382781d7020efdc962efb5682dcd35bda5033c
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-01-25 23:30:11 +0700

    Update user.php

--- user.php
+++ user.php
@@ -18,27 +18,13 @@ $tt_stat=true;
 $stat_ads=true;
 $data= [
  //we
-'Cocentz','Rully Efte','Hamzah Ismail','Lembu Gaming','Cybertz HackrzAllx','Ozil Oy','Alianz Jr.',
-'Adit yoo','Sunda G.R.T','Channel Cocentz','Sesha mn','Mas Boot','Dex Zizah','Apriliana Apriliana',
-'[ IRKOP ]','Martus Alae','Taleri Muloie','Lise Martis','Siti Berti','Slamet Fards','Vertiem Lolite',
-'THE JOET','Yogi Chiko','Jon Tendy','Panji Saputra','Michael Hermawan','Amelia Putri','Alexander Axio',
-'Kilaer Kioae','Umam Tuner','Alfa Jon','Jon Aldi','Dewi Septiani','Alex Siregar','Wendii Cimoot','Abdull',
-'Coki','behel.uje10@gmail.com','M.Khoirul Anwar','M. Khoirul Anwar','Belut Salmon','Selvi Ana','Syaifdin Joery',
-'Andri Mardat','KOCAK BGT','Papla Aqpkla','Muhammad pan','Irfan N','Becrotfr','Muhammad Khairil','Nando','Marsha',
-'Silva','Fardan','Nurdinn','C. DROID','Vika Cu','cocis','Gantari belantara putri','Reffer','Khomsun Bungsu',
-'ria syafrianti','Ahmad Rianto','slamet budi','Al Izza Anwar Ibrahim','Aziz Moh','M A R I U S Z','MARIUSZ',
-'Herideha','nyamukabate@gmail.com','Remon Andy','Roby Sinambela','Fahri Hafiz','Riki Sudarso05','Dunia Anak','Tohir',
-'Madavaka','Madavaka Seorng Noob','cecenet116@gmail.com','Ilham Abdull','timbangxote','Irfan Xns','mazazykusa@gmail.com',
-'Irfan Pay','Irfan Ws','Bang jon','Irfan xns','Irfan Admob','Irfan Ml','irfan warspears','Irfan gaming','tika seksi',
-'Bang Jon','bang jon','Irfan Admob','rico dwiwarsa','Dian Budi Rachmadhani','dhanidbr','nuyulterakir@gmail.com',
-'jokotiing@gmail.com','Yuna','Nempo Gakueun','Rolando Gonz','Aziz Kha','michael johan','Necrolizer','mike xza',
-'Max Wolo','Max wolo','Mardoni Muhamad',
+'Rully Efte',
  '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
 '23','24','25','26','27','28','29','30',
  '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
  '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
  '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
- 'Kolek Cau','Melin Syg','Channel Cocentz',
+ '77bda54df9cfa4c0cbb6c3c36229e844ca959a214c153491bd5155e660b5f968',
  //yg
  'Oblo Gaming2',
  //v
