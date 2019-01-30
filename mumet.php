d51bd7f9c428db796c108bb59d91fed76c920779
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-01-30 20:34:28 +0700

    Create mumet.php

---
+++ mumet.php
@@ -0,0 +1,59 @@
+<?php
+echo $putih."[$red!$putih]$red Warning !!!".$t;
+echo $putih."[🇲🇨] Language Indonesia".$t;
+echo $putih."[$red!$putih] Ini adalah program ilegal".$t;
+echo $putih."[$red!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
+echo $putih."[🇬🇧] Language English".$t;
+echo $putih."[$red!$putih] This is an illegal program".$t;
+echo $putih."[$red!$putih] Risk fully borne by the user".$t;
+
+$msgwe=$putih."[".$ijo."•".$putih."] Update News Script Watch&Earn VIP+$red v.2.0$putih".$t;
+$msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
+$msgtt=$putih."[*] Enjoy Your Life !! ".$t;
+$msgads=$putih."[$biru>$putih] Subscribe Channel YT Channel Cocentz & adi bordir 😁".$t;
+$msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
+$stat_dony=true;
+$st_dony=true;
+$yd_stat=false;
+$vip1=false;
+$weblock=[""];
+$ydblock=[""];
+$we_stat=true;
+$tt_stat=true;
+$stat_ads=true;
+$data= [
+ //we
+'Cocentz','alianz.hamza','Hamzah Ismail','Lembu Gaming','Cybertz HackrzAllx','Ozil Oy','Alianz Jr.',
+'Adit yoo','Sunda G.R.T','Channel Cocentz','Sesha mn','Mas Boot','Dex Zizah','Apriliana Apriliana',
+'[ IRKOP ]','Martus Alae','Taleri Muloie','Lise Martis','Siti Berti','Slamet Fards','Vertiem Lolite',
+'THE JOET','Yogi Chiko','Jon Tendy','Panji Saputra','Michael Hermawan','Amelia Putri','Alexander Axio',
+'Kilaer Kioae','Umam Tuner','Alfa Jon','Jon Aldi','Dewi Septiani','Alex Siregar','Wendii Cimoot','Abdull',
+'Coki','behel.uje10@gmail.com','M.Khoirul Anwar','M. Khoirul Anwar','Belut Salmon','Selvi Ana','Syaifdin Joery',
+'Andri Mardat','KOCAK BGT','Papla Aqpkla','Muhammad pan','Irfan N','Becrotfr','Muhammad Khairil','Nando','Marsha',
+'Silva','Fardan','Nurdinn','C. DROID','Vika Cu','cocis','Gantari belantara putri','Reffer','Khomsun Bungsu',
+'ria syafrianti','Ahmad Rianto','slamet budi','Al Izza Anwar Ibrahim','Aziz Moh','M A R I U S Z','MARIUSZ',
+'Herideha','nyamukabate@gmail.com','Remon Andy','Roby Sinambela','Fahri Hafiz','Riki Sudarso05','Dunia Anak','Tohir',
+'Madavaka','Madavaka Seorng Noob','cecenet116@gmail.com','Ilham Abdull','timbangxote','Irfan Xns','mazazykusa@gmail.com',
+'Irfan Pay','Irfan Ws','Bang jon','Irfan xns','Irfan Admob','Irfan Ml','irfan warspears','Irfan gaming','tika seksi',
+'Bang Jon','bang jon','Irfan Admob','rico dwiwarsa','Dian Budi Rachmadhani','dhanidbr','nuyulterakir@gmail.com',
+'jokotiing@gmail.com','Yuna','Nempo Gakueun','Rolando Gonz','Aziz Kha','michael johan','Necrolizer','mike xza',
+'Max Wolo','Max wolo','Mardoni Muhamad','Daniel Nyu','Riky muhamad','iwan muler','vijay jyoti','Vijay Kumar','HafIizh',
+'Hafiizh','fadhyl ramadana','Daniel Nguyễn','Gana dinero A través de páginas','Nhez Lhyne','Adi Betet','fadhyl ramad',
+'ime meme','Ilmu Apa Aja','Yogi Yoo','Iván Piola','Khamid Lillah',Rully Efte', nadeedazr@gmail.com',
+ '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
+'23','24','25','26','27','28','29','30',
+ '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
+ '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
+ '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
+ 'Kolek Cau','Melin Syg','Channel Cocentz',
+ //yg
+ 'Oblo Gaming2',
+ //v
+ 'Ozil Oy','Alianz X Cybertz',
+ //t
+ 'Cocentz','Hamzah Ismail','Kolek Cau','Ozil Oy','Sunda {G.R.T}',
+        ];
+$ydnews = ['tes','premium',];
+$watch = ['tes',];
+$veeu = ['tes',];
+$cword= ['tes','premium',];
