892ac0c79d6875af47d85cdc926d48d1d7bba239
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-01-30 17:23:31 +0700

    Update user.php

--- user.php
+++ user.php
@@ -5,7 +5,7 @@ echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
 $msgwe=$putih."[$red!$putih] Update News Script VIP$red v.2.0$putih".$t;
 $msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
 $msgtt=$putih."[*] Enjoy Your Life !! ".$t;
-$msgads=$putih."[$biru>$putih] Subscribe Channel YT Channel Cocentz & adi bordir 😁".$t;
+$msgads=$putih."[$biru>$putih] chanel".$t;
 $msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
 $stat_dony=true;
 $st_dony=true;
@@ -16,7 +16,9 @@ $ydblock=[""];
 $we_stat=true;
 $tt_stat=true;
 $stat_ads=true;
-$data= ['Z25299',];
+$data= [
+  //we 
+  'Rully Efte','nadeedazr@gmail.com',];
 $ydnews = ['tes','premium',];
 $watch = ['tes',];
 $veeu = ['tes',];
