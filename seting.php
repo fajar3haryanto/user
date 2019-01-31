6d7630a51d4d581c5cd9fcba1e983df3a1263c88
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-01-31 18:58:49 +0700

    Add files via upload

---
+++ seting.php
@@ -0,0 +1,32 @@
+<?php
+/* setting bot */
+$baca_plus=$red."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+creator:$ijo eftechanel$putih || code invite:$ijo FklNSRXggz".$t;
+$msg_baca= "[•] official eftechanel".$t."[•] info bagi yang mengalami kesalahan saat login".$t."[•] silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";
+$stat_baca=true;
+ 
+$ccu= $red."
+──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────".$pth."
+──── █─ █─█ █ █─▀█──── █─ █─▄ █─█ █▀█────
+──── ─▀ ─▀─ ▀ ▀──▀──── ─▀ ▀▀▀ ─▀─ ▀▀─────
+creator: ".$ijo."eftechanel".$t;
+$msg_ccu=$ijo."[•] Enjoy";
+$stat_ccu=true;
+$mc=$br."
+──── █▄─▄█ ▄▀▄ █▄─█ █▀▀ ▀▄─▄▀──── ▄▀ █─█ █▀▄ █▀▀────
+──── █─█─█ █─█ █─▀█ █▀▀ ──█────── █─ █─█ █▀█ █▀▀────
+──── ▀───▀ ─▀─ ▀──▀ ▀▀▀ ──▀────── ─▀ ─▀─ ▀▀─ ▀▀▀────
+".$pth."creator: ".$ijo."eftechanel ".$pth."|| ".$ijo."subcribe youtube eftechanel".$t.$pth."invite : ".$ijo."NCES2".$t;
+$msg_mc=$ijo."[•] enjoy your life";
+$stat_mc=true;
+$flash="$red
+ █▀──── █────── ▄▀▄──── ▄▀▀──── █────── ▄▀▀───── ▄▀▄
+ █▀──── █─▄──── █▀█──── ─▀▄──── █▀▄──── █─▀▌──── █─█
+ ▀───── ▀▀▀──── ▀─▀──── ▀▀───── ▀─▀──── ▀▀▀───── ─▀─
+$putih  creator :$ijo eftechanel$putih ||||| chanel :$turkis efte".$t;
+$msg_flash=$turkis."[•] flash go";
+$stat_flash=true;
+?>
