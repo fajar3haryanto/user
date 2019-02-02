a961f64efb6ad8a43bbbb9f989c7dfe9bd67b7f3
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-02 22:22:22 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,7 +2,6 @@
 echo $putih."[$red!$putih]$red Warning !!!".$t;
 echo $putih."[•] Ini adalah program ilegal".$t;
 echo $putih."[•] Resiko sepenuhnya di tanggung pengguna".$t;
-echo $putih."[•] This is an illegal program".$t;
 $msgwe=$putih."[".$ijo."•".$putih."] Script VIP+$red v.2.0$putih =>$red Yt$putih :$red eftechanel".$t;
 $msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
 $msgtt=$putih."[*] Enjoy Your Life !! ".$t;
@@ -19,7 +18,8 @@ $tt_stat=true;
 $stat_ads=true;
 $data= [
 //we
-'Muna Sosweety','Rully Efte','hamid Lilladh',
+'Muna Sosweety','Rully Efte','0',
+        '1','2','3','4','5','6',
 
         ];
 $ydnews = ['tes','eftechanel',];
