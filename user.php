e9ce64926793e67d9cf23afe45c8f09c7b9d34c4
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-05 08:42:21 +0700

    Update user.php

--- user.php
+++ user.php
@@ -5,7 +5,7 @@ echo $putih."[•] Resiko sepenuhnya di tanggung pengguna".$t;
 $msgwe=$putih."[".$ijo."•".$putih."] Script VIP+$red v.2.0$putih =>$red Yt$putih :$red eftechanel".$t;
 $msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
 $msgtt=$putih."[*] Enjoy Your Life !! ".$t;
-$msgads=$putih."[$biru>$putih] Subscribe Channel YT Channel".$t;
+$msgads=$putih."[$biru>$putih] Subscribe Channel YT EfteChannel".$t;
 $msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
 $stat_dony=true;
 $st_dony=true;
@@ -18,7 +18,7 @@ $tt_stat=true;
 $stat_ads=true;
 $data= [
 //we
-'mumet banget','Sondong Jpr','Muna Sosweety','Rully Efte','dje fourten','0',
+'mumet banget','Sondong Jpr','Muna Sosweety','Rully Efte','dje fourten','0','Ahmad Sakhowi',
         '1','2','3','4','5','6',
 //Yogo
         'Rully Efte','Nadeeda Zavi',
