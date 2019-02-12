6ab4f994a25ce73686d8b755849d2633b38d9f43
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-12 23:23:10 +0700

    Update user.php

--- user.php
+++ user.php
@@ -22,7 +22,8 @@ $data= [
 'mumet banget','Sondong Jpr','Muna Sosweety','Rully Efte','dje fourten','0','Ahmad Sakhowi','Hardian E.C',
         '1','2','3','4','5','6',
  //ve
-        'Efte chanel','Arya Sastra','Hoa Hunh','pletet aku','Si Montok','Nyoman Jobla','jogi laguna',
+        'Efte chanel','Arya Sastra','Hoa Hunh','pletet aku','Si Montok','Nyoman Jobla','jogi laguna','Dipa Putra',
+        'Dicky Sugara',
 //Yogo
         'Rully Efte','Nadeeda Zavi',
         ];
