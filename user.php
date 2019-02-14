3e21a5373c3f0570ffb02c7fc373c8ea26463a77
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-14 07:23:14 +0700

    Update user.php

--- user.php
+++ user.php
@@ -23,7 +23,7 @@ $data= [
         '1','2','3','4','5','6',
  //ve
         'Efte chanel','Arya Sastra','Hoa Hunh','pletet aku','Si Montok','Nyoman Jobla','jogi laguna','Dipa Putra',
-        'Dicky Sugara','kentuq kentuq',
+        'Dicky Sugara','kentuq kentuq','Abimanyu',
 //Yogo
         'Rully Efte','Nadeeda Zavi',
         ];
