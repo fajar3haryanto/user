edb0084b59b07ae4513a9c8055877d635df190d0
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-17 00:18:28 +0700

    Update user.php

--- user.php
+++ user.php
@@ -28,7 +28,7 @@ $data= [
 //Yogo
         'Rully Efte','Nadeeda Zavi',
         ];
-$ydnews = ['tes','eftechanel','Muna Sosweety','Telu Tiga',];
+$ydnews = ['Rully Efte',];
 $watch = ['tes',];
 $cword= ['tes','eftechanel',];
 $yogo= ['Rully Efte','Nadeeda Zavi','Abdu Fajar','mizan bagustian','Andy Ramdani',
