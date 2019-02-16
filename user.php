1d23f3abaf185e1e8a5a403d84553483e10d919b
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-16 23:33:03 +0700

    Update user.php

--- user.php
+++ user.php
@@ -32,5 +32,5 @@ $watch = ['tes',];
 $cword= ['tes','eftechanel',];
 $yogo= ['Rully Efte','Nadeeda Zavi','Abdu Fajar','mizan bagustian','Andy Ramdani',
        'Arin Arin','Yahya Hasan','Nade Eda','Rakha nurraffa','Htin Htin','Zulfikar Azaria','Dicky Sugara',
-        'YLKO YLKO','Janu Febrian',
+        'YLKO YLKO','Janu Febrian','Syed Moiz Uddin Ahmed',
        ];
