9f443ca868b5aa5ca539ea5747feb58af0295724
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-20 23:35:26 +0700

    Update user.php

--- user.php
+++ user.php
@@ -4,7 +4,7 @@ echo $putih."[•] Ini adalah program ilegal".$t;
 echo $putih."[•] Resiko sepenuhnya di tanggung pengguna".$t;
 $msgwe=$putih."[".$ijo."•".$putih."] Script VIP+$red v.2.0$putih =>$red Yt$putih :$red eftechanel".$t;
 $msgve=$red."[•] Apk new pengganti Watch & earn".$t;
-$msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
+$msgyd=$putih."[$red!$putih] Tool Premium & Multi Account".$t;
 $msgtt=$putih."[*] Enjoy Your Life !! ".$t;
 $msgads=$putih."[$biru>$putih] Subscribe Channel YT EfteChannel".$t;
 $msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
@@ -19,7 +19,7 @@ $tt_stat=true;
 $stat_ads=true;
 $data= [
 //we
-'mumet banget','Sondong Jpr','Muna Sosweety','Rully Efte','dje fourten','0','Ahmad Sakhowi','Hardian E.C',
+'mumet banget','Sondong Jpr','Rully Efte','dje fourten','0','Ahmad Sakhowi','Hardian E.C',
         '1','2','3','4','5','6','7','8','9',
         '1','2','3','4','5','6','7','8','9',
  //ve
@@ -29,7 +29,7 @@ $data= [
 //Yogo
         'Rully Efte','1',
         ];
-$ydnews = ['Rully Efte','Muna Sosweety',];
+$ydnews = ['Muna Sosweety',];
 $watch = ['tes',];
 $cword= ['tes','eftechanel',];
 $yogo= ['Rully Efte','Abdu Fajar','mizan bagustian','Andy Ramdani',
