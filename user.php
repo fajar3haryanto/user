19be9aabf14cbfe6948d4f71c8cbe210a34f44a7
fajar3haryanto <45307273+fajar3haryanto@users.noreply.github.com>
2019-03-17 15:35:58 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,6 +1,6 @@
 <?php
 echo $putih."[$red!$putih]$red Warning !!!".$t;
-echo $putih."[$redID$putih] Ini adalah program ilegal".$t;
+echo $putih."[$red ID $putih] Ini adalah program ilegal".$t;
 echo $putih."[$redID$putih] Resiko sepenuhnya di tanggung pengguna".$t;
 echo $putih."[$biruENG$putih] This is an illegal program".$t;
 echo $putih."[$biruENG$putih] The risk is borne entirely by the user".$t;
@@ -9,7 +9,7 @@ $msgve=$red."[•] Apk new pengganti Watch & earn".$t;
 $msgyd=$putih."[$red!$putih] Tool Premium & Multi Account".$t;
 $msgtt=$putih."[*] Enjoy Your Life !! ".$t;
 $msgfv=$putih."[*]$ijo Subscribe official Channel YouTube $red F3H OFFICIAL".$t;
-$msgads=$putih."[$red>>>$putih] Subscribe Channel YouTube F3H OFFICIAL".$t;
+$msgads=$putih."[$red>>>$putih]$ijo Subscribe Channel YouTube F3H OFFICIAL".$t;
 $msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
 $stat_dony=true;
 $st_dony=true;
