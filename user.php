c42e27371e609957613da25803b3ea992d9b13b8
fajar3haryanto <45307273+fajar3haryanto@users.noreply.github.com>
2019-04-05 22:56:12 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,42 +1,8 @@
-<?php
-echo $putih."[$red!$putih]$red Warning !!!".$t;
-echo $putih."[$red-$putih] Ini adalah program ilegal".$t;
-echo $putih."[$red-$putih] Resiko sepenuhnya di tanggung pengguna".$t;
-echo $putih."[$biru-$putih] This is an illegal program".$t;
-echo $putih."[$biru-$putih] The risk is borne entirely by the user".$t;
-$msgwe=$putih."[".$ijo."•".$putih."] Script VIP+$red v.2.0$putih =>$red YouTube$putih :$red F3H OFFICIAL".$t;
-$msgve=$red."[•] Apk new pengganti Watch & earn".$t;
-$msgyd=$putih."[$red!$putih] Tool Premium & Multi Account".$t;
-$msgtt=$putih."[*] Enjoy Your Life !! ".$t;
-$msgfv=$putih."[*]$ijo Subscribe official Channel YouTube $red F3H OFFICIAL".$t;
-$msgads=$putih."[$red>>>$putih]$kuning Subscribe Channel YouTube F3H OFFICIAL".$t;
-$msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
-$stat_dony=true;
-$st_dony=true;
-$yd_stat=true;
-$vip1=false;
-$weblock=[""];
-$ydblock=[""];
-$we_stat=true;
-$tt_stat=true;
-$stat_ads=true;
-$data= [
-//we
-'mumet banget','Sondong Jpr','Rully Efte','dje fourten','0','Ahmad Sakhowi','Hardian E.C',
-        '1','2','3','4','5','6','7','8','9',
-        '1','2','3','4','5','6','7','8','9',
- //ve
-        'Efte chanel','Arya Sastra','Hoa Hunh','pletet aku','Si Montok','Nyoman Jobla','jogi laguna','Dipa Putra',
-        'Dicky Sugara','kentuq kentuq','Abimanyu','Bilza G','ladang','iwanfsc','Aziz Cahyo','Kurniawan Zaty','Vijay Kumar',
-        'Job Padilla','Black Shark','Taufik','Susi Shine','Windii Rachmat','black shark','novan fardiansah','Tobing Cuy',
- //vf; 
-        'Nade Eda','Kaivan','Udin Cybernet','adipta apriyanto','Adipta Apriyanto','Rian 26','Rudi Wahyudi','Neni','ijah umel',
-        'Awashir Hammad','Ovay Waelah','Nury Waelah'.'muhammad hilmi husaini','youtube indo',
-//Yogo
-        'Rully Efte','1',
-        ];
-$ydnews = ['Rully Efte',];
-$watch = ['tes',];
-$cword= ['tes','eftechanel',];
-$yogo= ['Rully Efte',];
-$vefun= ['Rully Efte',];
+<? php
+$biru = "[1;34m";
+$turkis = "[1;36m";
+$ijo = "[92m";
+$putih = "[1;37m";
+$pink = "[1;35m";
+$red = "[1;31m";
+$kuning = "[1;33m";
