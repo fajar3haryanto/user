0df508d867558d0bb49314fe9094b9a17ba36fe4
fajar3haryanto <45307273+fajar3haryanto@users.noreply.github.com>
2019-04-05 22:59:56 +0700

    Update and rename user.php to user1.php

--- user1.php
+++ user1.php
@@ -1,8 +1,9 @@
-<? php
-$biru = "[1;34m";
+<?  php
+
+$   biru    =    "    [1; 34m" ;
 $turkis = "[1;36m";
 $ijo = "[92m";
 $putih = "[1;37m";
 $pink = "[1;35m";
 $red = "[1;31m";
-$kuning = "[1;33m";
+$  kuning   =   "   [1; 33m ;
